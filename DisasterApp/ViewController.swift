01fbbe0319e406fd530689cf9223fd149e104583
Nicholas Daniel Pratamaputra <nd8792@gmail.com>
2019-05-02 14:49:55 +0700

    first

---
+++ DisasterApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DisasterApp
+//
+//  Created by Nicholas Daniel Pratamaputra on 05/02/19.
+//  Copyright © 2019 Nicholas Daniel Pratamaputra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
