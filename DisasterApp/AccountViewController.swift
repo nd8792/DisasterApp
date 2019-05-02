03015c4fad443d928a327d79b261fb52d8a54169
Nicholas Daniel Pratamaputra <nd8792@gmail.com>
2019-05-02 15:34:41 +0700

    third

---
+++ DisasterApp/AccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AccountViewController.swift
+//  DisasterApp
+//
+//  Created by Nicholas Daniel Pratamaputra on 05/02/19.
+//  Copyright © 2019 Nicholas Daniel Pratamaputra. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
